b61cb23cbb0e39fe895bc31b003d0efbf90b676a
Laurence Li <li.laurence55@gmail.com>
2016-09-12 19:14:57 -0400

    Added jQuery

---
+++ main.js
@@ -0,0 +1,11 @@
+var navbarWidth = 50;
+var navbarColor = "#000000";
+
+body.append('<div id="navbar"></div>');
+var navbar = $('#navbar');
+
+navbar
+	.css("position", "absolute")
+	.css("top", "0px")
+	.css("left", "0px")
+	;
