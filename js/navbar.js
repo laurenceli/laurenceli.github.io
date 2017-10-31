044e2c7c4df3e38aadcfb6912ded04504e86d78e
Laurence Li <li.laurence55@gmail.com>
2017-10-31 00:12:46 -0400

    and more

--- js/navbar.js
+++ js/navbar.js
@@ -41,7 +41,7 @@ function drawNavBar(){
 }
 
 function drawLinksList(){
-	var linksText = ["ABOUT", "PROJECTS", "RESUME"];
+	var linksText = ["GITHUB", "RESUME"];
 	var linkElements = [];
 	var linkElementsCounter = 0;
 
@@ -73,7 +73,7 @@ function drawLinksList(){
 }
 
 function drawSocialList(){
-	var socialText = ["613 794 4859", "li.laurence55@gmail.com", "Github Profile", "LinkedIn Profile"];
+	var socialText = ["613 794 4859", "li.laurence55@gmail.com", "Github", "LinkedIn"];
 
 	navMenu.append('<div class="menuList" id="socialContainer"></div>')
 	socialContainer = $('#socialContainer');
